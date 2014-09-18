d37bad2ec83515e0cd1bc149696a1f28b776ac98
Geoff Noton <plasma@basalt.local>
2014-09-18 14:27:39 -0700

    Initial Commit

---
+++ BLELock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BLELock
+//
+//  Created by Geoff Noton on 9/18/14.
+//  Copyright (c) 2014 sublimeintervention. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
